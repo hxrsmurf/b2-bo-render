5bba27fba5a07c5bfa6d37648e210fe6ca9c3295
Kevin Murphy <kevin@hxrsmurf.info>
2017-04-27 03:28:17 -0400

    Fixed ffmpeg conversion, b2 upload, better null variables; using 64gb droplet with 20 CPUs

--- b2-do-render.sh
+++ b2-do-render.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-doIP=""
-doID=""
-doStatus=""
-doCreateDate=""
 
-logEmail=""
-sshKey=""
 b2Account=""
-b2Key=""
-doRegion="nyc2"
-doImage="debian-8-x64"
-doSize="512mb"
-#doSize="64gb"
-doName="test"
 b2Bucket=""
+b2Key=""
+doCreateDate=""
+doFinishDate=""
+doID=""
+doImage="debian-8-x64"
+doIP=""
+doName="test"
+doRegion="nyc2"
+doSize="64gb"
+doSSHstatus=""
+doStatus=""
+sshKey=""
 
 doctl compute droplet create $doName --size $doSize --image $doImage --region $doRegion --ssh-keys $sshKey
 
@@ -21,26 +21,26 @@ doCreateDate="$(date)"
 
 initDOstatus()
 {
-	doID="$(doctl compute droplet list | awk 'NR==2 {print $1}')"
-	doStatus="$(doctl compute droplet list | awk 'NR!=1 {print $11}')"
-	doIP="$(doctl compute droplet list | awk 'NR==2 {print $3'})"
-	doSSHstatus="$(nmap $doIP -PN -p ssh | egrep 'open|closed|filtered' | awk 'NR==1 {print $2}')"
-	getDOStatus
+        doID="$(doctl compute droplet list | awk 'NR==2 {print $1}')"
+        doStatus="$(doctl compute droplet list | awk 'NR!=1 {print $11}')"
+        doIP="$(doctl compute droplet list | awk 'NR==2 {print $3'})"
+        doSSHstatus="$(nmap $doIP -PN -p ssh | egrep 'open|closed|filtered' | awk 'NR==1 {print $2}')"
+        getDOStatus
 }
 
 getDOStatus()
 {
 while [ "$doStatus" != "active" ]; do
-	echo "Your droplet is being created..."
-	sleep 5
-	initDOstatus
-	break
+        echo "Your droplet is being created..."
+        sleep 5
+        initDOstatus
+        break
 done
 
 while [ "$doSSHstatus" != "open" ]; do
-	echo "We are waiting for SSH."
-	initDOstatus
-	break
+        echo "We are waiting for SSH."
+        initDOstatus
+        break
 done
 
 }
@@ -52,27 +52,27 @@ if [ "$doStatus" = "active" ]; then
         ssh -o "StrictHostKeyChecking no" root@$doIP wget https://bootstrap.pypa.io/get-pip.py
         ssh -o "StrictHostKeyChecking no" root@$doIP python get-pip.py
         ssh -o "StrictHostKeyChecking no" root@$doIP pip install -U pip setuptools
-	ssh -o "StrictHostKeyChecking no" echo "'deb http://ftp.de.debian.org/debian jessie-backports main' >> /etc/apt/sources.list"
+        ssh -o "StrictHostKeyChecking no" root@$doIP echo "'deb http://ftp.de.debian.org/debian jessie-backports main' >> /etc/apt/sources.list"
         ssh -o "StrictHostKeyChecking no" root@$doIP apt-get update
         ssh -o "StrictHostKeyChecking no" root@$doIP apt-get install git ffmpeg -yf
         ssh -o "StrictHostKeyChecking no" root@$doIP git clone https://github.com/Backblaze/B2_Command_Line_Tool.git
         echo "cd /root/B2_Command_Line_Tool/ && python setup.py install" | ssh -o "StrictHostKeyChecking no"  root@$doIP
         ssh -o "StrictHostKeyChecking no" root@$doIP b2 authorize-account $b2Account $b2Key
-        #ssh -o "StrictHostKeyChecking no" root@$doIP ffmpeg -i "bbb-4k.mp4" -threads 64 -vcodec libx264 -vf scale=1920x1080 -vb 10000K -acodec aac -ab 96k -ar 44100 -f mp4 "output.mp4"
-        #ssh -o "StrictHostKeyChecking no" root@$doIP b2 upload-file --threads0 $b2Bucket  output.mp4 output.mp4
+        ssh -o "StrictHostKeyChecking no" root@$doIP ffmpeg -i "bbb-4k.mp4" -threads 0 -vcodec libx264 -vf scale=1920x1080 -vb 7000K -acodec aac -ab 192k -ar 44100 -f mp4 "output.mp4"
+        ssh -o "StrictHostKeyChecking no" root@$doIP b2 upload-file --threads 0 $b2Bucket  output.mp4 output.mp4
 else
-	echo "Failed"
+        echo "Failed"
 fi
 }
 
 showComplete()
 {
-	echo "Congratulations, the Droplet has been created!"
-	echo "It can be accessed at this IP address: $doIP"
-	echo "We started at $doCreateDate"
-	doFinishDate="$(date)"
-	echo "We finished at $doFinishDate"
-	echo -e "Subject:Droplet Log \n\nCongratulations, the Droplet has been created! \n\nIt can be accessed at this IP address: $doIP. \n\nWe started at $doCreateDate \n\nWe finished at $doFinishDate." | sendmail -v $logEmail
+        echo "Congratulations, the Droplet has been created!"
+        echo "It can be accessed at this IP address: $doIP"
+        echo "We started at $doCreateDate"
+        doFinishDate="$(date)"
+        echo "We finished at $doFinishDate"
+        echo -e "Subject:Droplet Log \n\nCongratulations, the Droplet has been created! \n\nIt can be accessed at this IP address: $doIP. \n\nWe started at $doCreateDate \n\nWe finished at $doFinishDate." | sendmail -v YOUREMAIL
 }
 
 getDOStatus
